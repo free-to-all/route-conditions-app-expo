0fd5a5e2746adf5d14bcb2b6c63bd6a52f9a82ac
Yason Khaburzaniya <yason@hey.com>
2020-12-31 21:37:56 -0800

    use client for user auth action

--- models/ReportsClient.js
+++ models/ReportsClient.js
@@ -1,8 +1,9 @@
-import { format } from "date-fns";
+// @flow
+import {format} from "date-fns";
 
 const superagent = require( 'superagent' );
 
-export function indexReports(authToken, errorCallback, successCallback) {
+export function indexReports ( authToken:string, errorCallback, successCallback ) {
 
     superagent.get( 'http://192.168.1.20:3000/reports' )
         .set( {
@@ -12,17 +13,33 @@ export function indexReports(authToken, errorCallback, successCallback) {
         //TODO: handle bad response code. For example, should not trigger createRefreshReportsDoneAction, also
         // reduce must not allow null payload to ruin everything
         if ( err ) {
-            errorCallback(err);
-        } else if( res ) {
-            const reports = res.body.map(transformReport);
-            successCallback(reports);
+            errorCallback( err );
+        } else if ( res ) {
+            const reports = res.body.map( transformReport );
+            successCallback( reports );
         }
     } );
 }
 
-function transformReport(report) {
+//TODO: use command pattern here? instead of passing callbacks
+export function authenticateUser ( email: string, password: string, errorCallback, successCallback ) {
+    superagent.post( 'http://192.168.1.20:3000/authenticate' )
+        .send( {email: email, password: password} )
+        .set( {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        } ).end( ( err, res ) => {
+        if ( err ) {
+            errorCallback( err );
+        } else if ( res ) {
+            successCallback( res.body.auth_token );
+        }
+    } )
+}
+
+function transformReport ( report ) {
     const myReport = {...report}
-    const date = new Date(report.created_at);
-    myReport.created_at = format(date, "MMM do, yyyy H:mma");
+    const date = new Date( report.created_at );
+    myReport.created_at = format( date, "MMM do, yyyy H:mma" );
     return myReport;
 }
--- redux/slices/userSlice.js
+++ redux/slices/userSlice.js
@@ -1,4 +1,6 @@
 import {Dispatch} from "redux";
+import {authenticateUser as doAuth} from "../../models/ReportsClient";
+
 const superagent = require( 'superagent' );
 
 const {createSlice} = require( "@reduxjs/toolkit" );
@@ -7,7 +9,7 @@ const userSlice = createSlice( {
     name: 'user',
     initialState: {authToken: ''},
     reducers: {
-        userAuthenticating(state, action) {
+        userAuthenticating ( state, action ) {
             //TODO: display "Authenticating..."
         },
         userAuthenticated ( state, action ) {
@@ -19,16 +21,16 @@ const userSlice = createSlice( {
 export function authenticateUser ( email: string, password: string ) {
     return ( dispatch: Dispatch ) => {
         dispatch( (userSlice.actions.userAuthenticating( email )) );
-        superagent.post( 'http://192.168.1.20:3000/authenticate' )
-            .send( {email: email, password: password} )
-            .set( {
-                "Accept": "application/json",
-                "Content-Type": "application/json",
-            } ).end( ( err, res ) => {
-            if ( res ) dispatch( userSlice.actions.userAuthenticated( res.body.auth_token ) );
-        } )
+        doAuth( email, password,
+            ( error ) => {
+
+            },
+            ( authToken ) => {
+                dispatch( userSlice.actions.userAuthenticated( authToken ) );
+            }
+        )
     }
 }
 
-export const { userAuthenticated, userAuthenticating } = userSlice.actions;
+export const {userAuthenticated, userAuthenticating} = userSlice.actions;
 export default userSlice.reducer;
